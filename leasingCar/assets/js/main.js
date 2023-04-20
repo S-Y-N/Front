41d11107d14e876389ad0906bae3fdb80bafdc10
Yevhen Trystan <kingguru@ukr.net>
2023-04-20 17:36:46 +0300

    leasing car site without adapt

---
+++ leasingCar/assets/js/main.js
@@ -0,0 +1,22 @@
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function (el) {
+    el.addEventListener('click', open)
+})
+
+function open(event) {
+    const isTarget = event.currentTarget;
+    const btn = isTarget.dataset.button;
+
+    tabItem.forEach(function (item) {
+        item.classList.remove('tabs__btn-item--active');
+    });
+
+    isTarget.classList.add('tabs__btn-item--active');
+
+    tabContent.forEach(function (item) {
+        item.classList.remove('tabs__content-item--active')
+    });
+    document.querySelector(`#${btn}`).classList.add('tabs__content-item--active');
+}
